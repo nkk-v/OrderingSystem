b6e5995c1472cfd1dc182dc63f0ce8e920ed94b6
nkk-v <nikvillanueva1114@gmail.com>
2025-04-21 14:57:19 +0800

    CommitChanges

---
+++ OrderingSystem/ViewModels/CartVIewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderingSystem.ViewModels
+{
+    public class CartVIewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+    }
+}
--- OrderingSystem/obj/Debug/net8.0/OrderingSystem.AssemblyInfo.cs
+++ OrderingSystem/obj/Debug/net8.0/OrderingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78734a0717b62e2e370f9b0ad83226a6d0889e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
